6b694ed4eb674666b580d991b1647fda575bab0d
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-05-05 22:08:51 +0700

    get notification via req.body

--- backend/controller/reserve.controller.js
+++ backend/controller/reserve.controller.js
@@ -7,7 +7,7 @@ module.exports = async (req, res) => {
     const { date } = await Parking.findOne({ name: req.params.name });
     const resp = await Parking.updateOne(
       { name: req.params.name },
-      { token: req.params.token, date: date ? date : Date.now() }
+      { token: req.body?.token || "", date: date ? date : Date.now() }
     );
     res.json(resp);
   } catch (err) {
