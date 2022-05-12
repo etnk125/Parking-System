d57fcdfb7493fc45e933823b6f76854659a3f5f2
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-05-12 13:59:43 +0700

    init vip

---
+++ Pi/vip-parking/chatbot.py
@@ -0,0 +1,68 @@
+# import
+from flask import Flask, request, make_response, jsonify
+import RPi.GPIO as GPIO
+import os
+import json
+##
+
+
+# led = [31, 33, 35, 37]
+ir = [32, 36, 38, 40]
+
+# gpio init
+
+
+def gpio_init():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
+    # GPIO.setup(7, GPIO.OUT)
+
+
+def gpio_set_input():
+    for i in ir:
+        GPIO.setup(i, GPIO.IN)
+
+
+# create flask app
+app = Flask(__name__)
+log = app.logger
+
+# recieve request from webhook
+
+
+@app.route("/", methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+#    print(req.get('queryResult').get('parameters').get('place'))
+
+    try:
+        action = req.get('queryResult').get('intent').get('displayName')
+    except AttributeError:
+        return 'json error'
+    # action switcher
+    res = ""
+    if action == 'Check':
+        res = get_remain()
+    else:
+        log.error('Unexpected action.')
+
+    print('Action: ' + str(action))
+    print('Response: ' + res)
+    # print(json.dumps(req, indent=2, sort_keys=True))
+    # return response
+    return make_response(jsonify({'fulfillmentText': res}))
+
+
+def get_remain():
+    max_slot = len(ir)
+    remain = len([sensor for sensor in ir if GPIO.input(sensor) == GPIO.HIGH])
+
+    return f'remaining: {remain}/{max_slot}'
+
+
+# run flask app
+if __name__ == '__main__':
+    gpio_init()
+    gpio_set_input()
+    app.run(host='0.0.0.0', debug=True, port=int(
+        os.environ.get('PORT', '5000')))
---
+++ Pi/vip-parking/index.py
@@ -0,0 +1,75 @@
+# Natthawee Koengfak 6213125
+# https://docs.google.com/spreadsheets/d/1-Hp5QSB9J4o3Ljcb1Mn9XatZvVPePCXGoufBd5jaR5k/edit?usp=sharing
+import time
+from datetime import datetime
+
+
+import RPi.GPIO as GPIO
+
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.virtual import viewport, sevensegment
+
+
+# Initialize Netpie information
+NETPIE_HOST = "broker.netpie.io"
+CLIENT_ID = "9c389ca8-6e95-4063-916d-52f284b5684d"  # YOUR CLIENT ID
+DEVICE_TOKEN = "tYEw2842zc1iWR6zHHMhcrtpeTYaaCN2"  # YOUR TOKEN
+
+
+# gpio init
+def gpio_init(outp=[], inp=[]):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    for i in outp:
+        GPIO.setup(i, GPIO.OUT)
+    for i in inp:
+        GPIO.setup(i, GPIO.IN)
+
+
+# connect ggs
+def connect(SheetName, GSheet_OAUTH_JSON, worksheet_name, scope):
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        GSheet_OAUTH_JSON, scope)
+    client = gspread.authorize(credentials)
+    worksheet = client.open(SheetName).worksheet(worksheet_name)
+    return worksheet
+
+
+def toggle(pin, condition=False):
+    GPIO.output(pin, GPIO.HIGH if condition else GPIO.LOW)
+
+
+def main():
+    main_class = Main()
+    main_class.run()
+
+
+class Main:
+    # led
+    ir = [32, 36, 38, 40]
+    led = [31, 33, 35, 37]
+
+    def __init__(self):
+        # using gpio
+        gpio_init(outp=self.led, inp=self.ir)
+
+        # 7seg
+        self.serial = spi(port=0, device=0, gpio=noop())
+        self.device = max7219(self.serial, cascaded=1)
+        self.seg = sevensegment(self.device)
+
+    def run(self):
+        while True:
+            sum = 0
+            for i, e in enumerate(self.ir):
+                toggle(self.led[i], GPIO.input(e))
+                sum += GPIO.input(e)
+
+            self.seg.text = str(sum)
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
